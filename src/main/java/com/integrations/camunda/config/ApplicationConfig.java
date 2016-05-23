e64fe149d9242c0fcc6e95b358251c24dcd44699
Max Mendoza <mmendoza@24hourfit.com>
2016-05-23 10:40:05 -0700

    changes

---
+++ src/main/java/com/integrations/camunda/config/ApplicationConfig.java
@@ -0,0 +1,8 @@
+package com.integrations.camunda.config;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("v1")
+public class ApplicationConfig extends Application {
+}
